282d47314a1ea7db980ddaf53ddde96e2205b3dc
htejadag <htejadag25@gmail.com>
2025-01-18 18:31:56 -0500

    Commit Inicial

---
+++ EF/src/test/java/unu/EF/EfApplicationTests.java
@@ -0,0 +1,13 @@
+package unu.EF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
